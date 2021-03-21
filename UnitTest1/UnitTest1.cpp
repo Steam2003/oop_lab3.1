1cf8f38f593d659a1c4c30146c90e7f6d6760153
Іван <gendalf2003@gmail.com>
2021-03-21 16:48:37 +0200

    lab3.1

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Triad.cpp"
+#include "../Triad.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Triad a(1, 2, 3);
+			int test = a.GetHours();
+			Assert::AreEqual(test, 1);
+		}
+	};
+}
